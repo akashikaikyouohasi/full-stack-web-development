deb33fe75a9e581837cc655ef3a34c0532f13fd3
anikinthos <hayatata@live.jp>
2024-12-30 18:20:23 +0900

    add: /api/hello/backendのAPIを実装

--- app/backend/api/hello/views.py
+++ app/backend/api/hello/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+class Backend(APIView):
+    def get(self, request, format=None):
+        # Getリクエストを受け取った時の処理
+        return Response({"message": "backend"})
