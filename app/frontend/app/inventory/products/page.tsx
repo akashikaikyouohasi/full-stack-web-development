64589d8ea76412cc88c03331fdd42e943b4fc19d
anikinthos <hayatata@live.jp>
2025-02-07 08:42:54 +0900

    feat: 動的描画の導入

--- app/frontend/app/inventory/products/page.tsx
+++ app/frontend/app/inventory/products/page.tsx
@@ -1,8 +1,51 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import productsData from "./sample/dummy_products.json"
+
+type ProductData = {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+}
+
 export default function Page() {
+    // 読子データを保持
+    const [data, setData] = useState<Array<ProductData>>([])
+
+    useEffect(() => {
+        setData(productsData);
+    }, [])
+
     return (
-        <div>
+        <>
             <h2>商品一覧</h2>
-            <p>商品の一覧を表示</p>
-        </div>
+            <button>商品を追加する</button>
+            <table>
+                <thead>
+                    <tr>
+                        <th>商品ID</th>
+                        <th>商品名</th>
+                        <th>単価</th>
+                        <th>説明</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((data: any) => (
+                    <tr key={data.id}>
+                        <td>{data.id}</td>
+                        <td>{data.name}</td>
+                        <td>{data.price}</td>
+                        <td>{data.description}</td>
+                        <td>
+                            <button>更新・削除</button>
+                        </td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
     )
 }
