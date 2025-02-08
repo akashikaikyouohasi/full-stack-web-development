34ac041a5d89d155ac2b9c298f5ce3a9f6f13bc9
anikinthos <hayatata@live.jp>
2025-02-08 10:20:53 +0900

    feat: 登録用入力フォーム追加

--- app/frontend/app/inventory/products/page.tsx
+++ app/frontend/app/inventory/products/page.tsx
@@ -19,10 +19,28 @@ export default function Page() {
         setData(productsData);
     }, [])
 
+    // 新規登録処理、新規登録行の表示状態を保持
+    const [shownNewRow, setShownNewRow] = useState(false);
+    const handleShowNewRow = (event: React.MouseEvent<HTMLElement>) => {
+        event.preventDefault();
+        setShownNewRow(true)
+    };
+    const handleAddCancel = (event: React.MouseEvent<HTMLElement>) => {
+        event.preventDefault();
+        setShownNewRow(false)
+    };
+    const handleAdd = (event: React.MouseEvent<HTMLElement>) => {
+        event.preventDefault();
+        //バックエンドを使用した登録処理を呼ぶ
+        setShownNewRow(false)
+    }
+
+
+
     return (
         <>
             <h2>商品一覧</h2>
-            <button>商品を追加する</button>
+            <button onClick= { handleShowNewRow }>商品を追加する</button>
             <table>
                 <thead>
                     <tr>
@@ -35,6 +53,16 @@ export default function Page() {
                     </tr>
                 </thead>
                 <tbody>
+                    { shownNewRow ? (
+                        <tr>
+                            <td></td>
+                            <td><input type="text" /></td>
+                            <td><input type="number" /></td>
+                            <td><input type="text" /></td>
+                            <td></td>
+                            <td><button onClick={handleAddCancel}>キャンセル</button><button onClick={handleAdd}>登録する</button></td>
+                        </tr>
+                    ) : ""}
                     {data.map((data: any) => (
                     <tr key={data.id}>
                         <td>{data.id}</td>
