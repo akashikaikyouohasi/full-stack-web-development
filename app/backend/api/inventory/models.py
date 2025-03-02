2ad9cc0166e0ff607d643ba8247ed320f007531c
anikinthos <hayatata@live.jp>
2025-03-02 11:02:21 +0900

    add: 商品マスタテーブルの追加

--- app/backend/api/inventory/models.py
+++ app/backend/api/inventory/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model): # モデル作成時にはDjangoのModelクラスの継承が必要
+    """
+    商品
+    """
+    # IDは自動で付与されるので定義不要。
+    name = models.CharField(max_length=100, verbose_name='商品名') # verbose_nameは論理名
+    price = models.IntegerField(vaebose_name='価格')
+    description = models.TextField(verbose_name='商品説明', null=True, blank=True)
+
+    class Meta:
+        """
+        メタ情報
+        """
+        db_table = 'product'
+        verbose_name = '商品'
