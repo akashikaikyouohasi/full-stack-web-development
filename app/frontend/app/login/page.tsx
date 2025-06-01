f8fabd3e3074bf36a9486531e2f12b26ec6b3dc6
anikinthos <hayatata@live.jp>
2025-06-01 14:02:46 +0900

    add: ログイン画面のつなぎこみ

--- app/frontend/app/login/page.tsx
+++ app/frontend/app/login/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import axios from "axios";
 import {
     createTheme,
     Box,
@@ -8,7 +9,8 @@ import {
     TextField,
     ThemeProvider,
     Typography,
-    Button
+    Button,
+    accordionSummaryClasses
 } from "@mui/material";
 
 import { useRouter } from "next/navigation";
@@ -26,6 +28,7 @@ export default function Page() {
         handleSubmit,
         formState: { errors },
     } = useForm();
+    const [authError, setAuthError] = useState("");
     const router = useRouter();
 
     const defaultTheme = createTheme();
@@ -40,7 +43,14 @@ export default function Page() {
     };
 
     const handleLogin = (data: FormData) => {
-        router.push("/inventory/products");
+        axios
+            .post("/api/inventory/login", data)
+            .then((response) => {
+               router.push("/inventory/products"); 
+            })
+            .catch(function (error){
+                setAuthError("ユーザー名またはパスワードに誤りがあります。");
+            });
     };
     
     return (
@@ -59,6 +69,11 @@ export default function Page() {
                         ログイン
                     </Typography>
                     <Box component="form" onSubmit={handleSubmit(onSubmit)}>
+                        {authError && (
+                            <Typography variant="body2" color="error">
+                                {authError}
+                            </Typography>
+                        )}{""}
                         <TextField
                             type="text"
                             id="username"
